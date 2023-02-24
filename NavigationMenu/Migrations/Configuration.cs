a683251d78a1e0d54d033c290829a36cc4265444
milad.jahandide <milad.jahandide@gmail.com>
2023-02-24 03:10:09 -0500

    Complete project added

---
+++ NavigationMenu/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Data.SQLite.EF6.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationMenu.Migrations
+{
+    internal class Configuration : DbMigrationsConfiguration<NavigationMenu.Models.NavigationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("System.Data.SQLite", new SQLiteMigrationSqlGenerator());// the Golden Key
+        }
+
+        protected override void Seed(NavigationMenu.Models.NavigationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
